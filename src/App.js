572cd4495fd1c5d3172001d61cb6269843c37c70
Shree Hari <shreeh1@umbc.edu>
2019-04-03 01:10:15 -0400

    parse
    First comit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import XMLParser from 'react-xml-parser'
 
 class App extends Component {
+  constructor(){
+    super();
+    console.log('hi')
+  const api_call = fetch(`http://export.arxiv.org/api/query?search_query=ti:${'electron'}&sortBy=lastUpdatedDate&sortOrder=ascending`).then((data)=>{
+    // data.xml((xml)=>{
+    //   console.log(xml)
+    // })  
+    // debugger;
+  console.log(data);
+  return data.text()
+  }).then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+  .then(data => {
+    
+    console.log(data)
+  
+  })
+  console.log('************XML below*********');
+  }
   render() {
     return (
       <div className="App">
