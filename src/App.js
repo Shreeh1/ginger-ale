e40ac32983b8765247da9e3a2ef356e07930b5df
Shree Hari <shreeh1@umbc.edu>
2019-04-03 17:59:56 -0400

    displaying

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Articles from './components/Articles'
+import { format } from 'path';
+// import $ from 'jquery';
+// import XmlToJson from './components/XmlToJson' 
 
 class App extends Component {
   constructor(){
     super();
     
-  fetch(`http://export.arxiv.org/api/query?search_query=ti:${'psychiatry'}&sortBy=lastUpdatedDate&sortOrder=ascending`).then((data)=>{
-
+  fetch(`http://export.arxiv.org/api/query?search_query=ti:${'therapy'}&sortBy=lastUpdatedDate&sortOrder=ascending`).then((data)=>{
   console.log(data);
   return data.text()
   }).then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
@@ -17,13 +18,26 @@ class App extends Component {
     // var xml = require('fs').readFileSync(data, 'utf8');
     // var result = convert.xml2json(xml, {compact: true, spaces: 4});
     console.log(data);
+    var title = data.getElementsByTagName("title");
+    var txt;
+    for (var i = 1; i < title.length; i++){
+      txt += title[i].childNodes[0].nodeValue + "<br>";
+    }
+    document.getElementById("demo").innerHTML = txt;
+    
+    // var json = XmlToJson.xmlToJson(data)
+    // console.log(json);
   })
-  
-  }
+}
+handleChange() {
+  var x = document.getElementById("demo").text
+  console.log(x)
+}
+
   render() {
     return (
       <div>
-        <h1> Hello World</h1>
+        <a id = "demo" href = "#" onClick= {this.handleChange}> </a>
       </div>
     );
   }
