8efb6142bec8539061e7cc93012f3d0460660a70
Shree Hari <shreeh1@umbc.edu>
2019-04-04 15:24:58 -0400

    author fetch

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ class App extends Component {
     super();
     this.state = {data: null};
     this.getData()
+    this.getauth_articles = this.getauth_articles.bind(this);
 }
 
-getData(){
+getData() {
   fetch(`http://export.arxiv.org/api/query?search_query=ti:${'therapy'}&sortBy=lastUpdatedDate&sortOrder=ascending`)
   .then(data=>data.text()).then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
     .then(data => {
       var entry = data.getElementsByTagName("entry");
       let elems = []
       for(let i=0;i<entry.length;i++){
-        // console.log(entry[i].getElementsByTagName("id")[0].textContent)
+        console.log(entry);
         let elem = <div key={i} id={entry[i].getElementsByTagName("id")[0].textContent} onClick={this.handleChange}>{entry[i].getElementsByTagName("title")[0].textContent}</div>;
         elems.push(elem);
       }
@@ -33,19 +34,40 @@ handleChange(evt) {
     .then(data => {
       var summ = data.getElementsByTagName("summary");
       var auth = data.getElementsByTagName("author");
-      let elems;
+      let elems1 = [];
       let name = []
       console.log(auth.length)
-      for (var i = 0; i < auth.length; i++ ){
+      for (let i = 0; i < auth.length; i++ ){
         console.log(auth[i].textContent)
+        // let elem1 = <div key={i} id={auth[i].textContent} onClick={this.getauth_articles}>{auth[i].textContent}</div>;
+        // elems1.push(elem1);
         name += auth[i].textContent;
+        // console.log(elem)
+      
       }
       document.getElementById("demo").innerHTML = summ[0].textContent + name;
-      
       // window.history.pushState({}, "page 2", "bar.html");
+      // this.setState({data: elems1});
+
     })
+  }
+  getauth_articles(evt) {
+    console.log(evt)
+    // fetch(`https://arxiv.org/search/q-bio?searchtype=author&query=`)
+    // .then(data=> data.text()).then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+    //   .then(data => {
+    //     var arti = data.getElementsByTagName("author");
+    //     let titles = []
+    //     for(let i=0;i<arti.length;i++){
+    //       let elem = <div key={i} id={arti[i].getElementsByTagName("id")[0].textContent} onClick={this.handleChange}>{arti[i].getElementsByTagName("author")[0].textContent}</div>;
+    //       titles.push(elem);
+    //     }
+    //     console.log(titles)
+
+    // })
+  }
   
-}
+
 
   render() {
     let data = this.state.data;
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -5,7 +5,74 @@ class Summary extends Component {
   render() {
     return (
       <div>
-        
+        import React, { Component } from 'react';
+import './App.css';
+import { format } from 'path';
+// import $ from 'jquery';
+// import XmlToJson from './components/XmlToJson' 
+
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      data_res: []
+    }
+
+  // fetch(`http://export.arxiv.org/api/query?search_query=ti:${'therapy'}&sortBy=lastUpdatedDate&sortOrder=ascending`).then((data)=>{
+  // console.log(data);
+  // return data.text()
+  // }).then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+  // .then(data => {
+  
+  //   console.log(data);
+  //   var title = data.getElementsByTagName("title");
+  //   // var summary = data.getElementsByTagName("summary");
+
+    // var txt;
+    // txt += "<a href = # onmouseover= {this.handleChange}>Hello </a>";
+    // // for (var i = 1; i < title.length; i++){
+    //   // txt += "<a href= id =" +title[i].childNodes[0].nodeValue + " onClick={this.handleChange}>"+ title[i].childNodes[0].nodeValue + "</a><br>";
+    // // }
+  //   document.getElementById("demo").innerHTML = txt;
+    
+  //   // var json = XmlToJson.xmlToJson(data)
+  //   // console.log(json);
+  // })
+}
+  componentDidMount() {
+    fetch(`http://export.arxiv.org/api/query?search_query=ti:${'therapy'}&sortBy=lastUpdatedDate&sortOrder=ascending`)
+    .then(data => {
+      var title = data.getElementsByTagName("title");
+      this.setState({data_res: title})});
+}
+  handleChange(e) {
+  // var x = document.getElementById("demo").text
+  console.log("helo")
+}
+
+  render() {
+    const { data_res } = this.state;
+    return (
+
+      <div id = "demo">
+        {
+          // var title = data.getElementsByTagName("title");
+          data_res.map(title =>
+            <a href='#'>Hello </a>
+          )
+
+        }
+        <p> Hello </p>
+        {/* <a id = "demo" href = "#" onClick= {this.handleChange}> </a> */}
+      </div>
+      
+    );
+  }
+}
+
+export default App;
+
       </div>
     );
   }
